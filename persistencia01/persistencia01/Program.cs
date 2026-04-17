13caf6a8684fd1e14d7ae5701708104510723ead
yeerett <yeretvillalonga@gmail.com>
2026-04-17 14:21:09 -0400

    modifiqué una linea

--- persistencia01/persistencia01/Program.cs
+++ persistencia01/persistencia01/Program.cs
@@ -14,7 +14,7 @@ namespace persistencia01
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("TALLER SECCION B");
 			
 			// TODO: Implement Functionality Here
 			
