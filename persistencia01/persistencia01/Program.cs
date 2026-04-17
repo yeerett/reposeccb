91c2a41e11cb546373c1c991d720644546f8d6f5
yeerett <yeretvillalonga@gmail.com>
2026-04-17 14:18:46 -0400

    creando el proyecto persistencia

---
+++ persistencia01/persistencia01/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: usuario
+ * Fecha: 17/4/2026
+ * Hora: 2:16 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace persistencia01
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
